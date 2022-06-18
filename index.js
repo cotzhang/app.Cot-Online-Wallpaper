36f5eddc791201492c5b9daafe0e2de1ec50c5d0
cotzhang <3063925756@qq.com>
2022-06-18 11:59:42 +0800

    Completed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const glasstron = require('glasstron');
 const electron = require('electron');
+const path = require('path')
 const remote = require("@electron/remote/main")
+const Store = require('electron-store');
+const store = new Store();
+let appTray = null;
 remote.initialize();
 let win;
 electron.app.commandLine.appendSwitch("enable-transparent-visuals");
@@ -18,10 +22,11 @@ electron.app.on('ready', () => {
 
 function spawnWindow(){
 	win = new glasstron.BrowserWindow({
-		width: 600,
-		height: 400,
+		width: 450,
+		height: 294,
 		frame: false,
 		resizable:false,
+		show:false,
 		transparent:true,
 		hasShadow:true,
 		webPreferences:{
@@ -37,5 +42,35 @@ function spawnWindow(){
 	//win.setAlwaysOnTop("alwaysOnTop")
 	//win.webContents.openDevTools({mode:"detach"})
 	remote.enable(win.webContents)
+  var trayMenuTemplate = [
+  {
+        label: "Refresh",        
+        click: function() {
+            win.webContents.send('refresh')     
+        } 
+    },
+    {type: 'separator'},
+    {
+        label: "Configuration",        
+        click: function() {
+            win.show();         
+        } 
+    },{
+        label: "Exit",        
+        click: function() {
+            electron.app.exit();        
+        } 
+    }
+    ];
+    let iconPath = path.join(__dirname, "./icon.ico");
+    let appTray = new electron.Tray(iconPath);
+    console.log(iconPath);
+    const contextMenu = electron.Menu.buildFromTemplate(trayMenuTemplate);
+    appTray.setToolTip("Cot Online Wallpaper");
+    appTray.setContextMenu(contextMenu);
+    if(store.get('firstrun')!="no"){
+    	win.show();
+		store.set('firstrun', 'no');
+    }
 	return win;
 }
